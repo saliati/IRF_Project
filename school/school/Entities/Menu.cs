d3eed0300a491243b693f7beec7b66ee90593da2
Attila Salamon <attila.salamon@stud.uni-corvinus.hu>
2020-12-15 02:09:18 +0100

    Next

---
+++ school/school/Entities/Menu.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace school.Entities
+{
+    class Menu
+    {
+        public string Name { get; set; }
+        public string Leves { get; set; }
+        public string Foetel { get; set; }
+        public string Desszert { get; set; }
+        public string Ar { get; set; }
+        public string Kaloria { get; set; }
+
+        public Menu(string name, string leves, string foetel, string desszert, string ar, string kaloria)
+        {
+            Name = name;
+            Leves = leves;
+            Foetel = foetel;
+            Desszert = desszert;
+            Ar = ar;
+            Kaloria = kaloria;
+        }
+    }
+}
--- school/school/Form1.cs
+++ school/school/Form1.cs
@@ -20,13 +20,62 @@ namespace school
 
             pictureBox1.Width = pictureBox1.Image.Width;
             List<Etterem> e = getAllEtterem("restaurants.xml");
+            List<Menu> m = getMenu("1.xml"); //problémás
             EtteremXML();
         }
 
         private List<Etterem> getMenu(string id)
         {
-            List<Etterem> e = new List<Etterem>();
+            //?
+            List<Menu> m = new List<Menu>();
             XmlReader reader = XmlReader.Create(id + ".xml");
+            reader.ReadToFollowing("Menu");
+            do
+            {
+                reader.GetAttribute("name");
+                string name = reader.Value;
+                reader.ReadToFollowing("leves");
+                string leves = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("foetel");
+                string foetel = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("desszert");
+                string desszert = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("ar");
+                string ar = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("kaloria");
+                string kaloria = reader.ReadElementContentAsString();
+
+                m.Add(new Menu(name, leves, foetel, desszert, ar, kaloria));
+            } 
+            while (reader.ReadToFollowing("menu"));
+            return m;
+        }
+
+        private List<Etterem> getAllEtterem(string file) 
+        {
+            List<Etterem> e = new List<Etterem>();
+            XmlReader reader = XmlReader.Create("restaurants.xml");
+            reader.ReadToFollowing("restaurant");
+            do
+            {
+                reader.GetAttribute("Id");
+                string id = reader.Value;
+                reader.GetAttribute("name");
+                string name = reader.Value;
+                reader.ReadToFollowing("address");
+                string address = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("cuisine");
+                string cuisine = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("phoneno");
+                string phoneno = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("feedback");
+                string feedback = reader.ReadElementContentAsString();
+
+                e.Add(new Etterem(id, name, address, cuisine, phoneno, feedback));
+            } 
+            
+            while (reader.ReadToFollowing("restaurant"));
+            return e;
         }
 
             public void EtteremXML()
