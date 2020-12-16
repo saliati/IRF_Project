be150931dfa9e51d8e12604290b036996c9ad3dd
Attila Salamon <attila.salamon@stud.uni-corvinus.hu>
2020-12-16 18:11:59 +0100

    Menu

--- Restaurant_Ati/Restaurant_Ati/Entities/Etterem.cs
+++ Restaurant_Ati/Restaurant_Ati/Entities/Etterem.cs
@@ -8,5 +8,91 @@ namespace Restaurant_Ati.Entities
 {
     class Etterem
     {
+        private string Id { get; set; }
+        private string Name { get; set; }
+        private string Address { get; set; }
+        private string Type { get; set; }
+        private string PhoneNo { get; set; }
+        private string Feedback { get; set; }
+
+        private Nyitvatartas Ny { get; set; }
+
+        private List<Menu> Menus { get; set; }
+
+        public Etterem(string id, string name, string address, string type, string phoneno, string feedback, string nyitvatartas)
+        {
+            Id = id;
+            Name = name;
+            Address = address;
+            Type = type;
+            PhoneNo = phoneno;
+            Feedback = feedback;
+            Menus = readMenus();
+            Ny = new Nyitvatartas(nyitvatartas);
+
+        }
+        public string getId()
+        {
+            return Id;
+        }
+        public string getFeedback()
+        {
+            return Feedback;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+        public Nyitvatartas getNyitvatartas()
+        {
+            return Ny;
+        }
+        public Menu getMenuByName(string name)
+        {
+            foreach (Menu menu in Menus)
+            {
+                if (menu.getName() == name)
+                {
+                    return menu;
+                }
+            }
+            return null;
+        }
+        private List<Menu> readMenus()
+        {
+
+            List<Menu> m = new List<Menu>();
+            XmlReader reader = XmlReader.Create(Id + ".xml");
+            reader.ReadToFollowing("menu");
+            do
+            {
+                reader.MoveToAttribute("name");
+                string name = reader.Value;
+                reader.ReadToFollowing("leves");
+                string leves = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("foetel");
+                string foetel = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("desszert");
+                string desszert = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("ar");
+                string ar = reader.ReadElementContentAsString();
+                reader.ReadToFollowing("kaloria");
+                string kaloria = reader.ReadElementContentAsString();
+
+                m.Add(new Menu(name, leves, foetel, desszert, ar, kaloria));
+            }
+            while (reader.ReadToFollowing("menu"));
+            return m;
+        }
+        public Stars getStar()
+        {
+            if (double.Parse(Feedback) < 1) return Stars.zero;
+            if (double.Parse(Feedback) < 2) return Stars.one;
+            if (double.Parse(Feedback) < 3) return Stars.two;
+            if (double.Parse(Feedback) < 4) return Stars.three;
+            if (double.Parse(Feedback) < 5) return Stars.four;
+
+            return Stars.five;
+        }
     }
 }
--- Restaurant_Ati/Restaurant_Ati/Entities/Menu.cs
+++ Restaurant_Ati/Restaurant_Ati/Entities/Menu.cs
@@ -8,5 +8,49 @@ namespace Restaurant_Ati.Entities
 {
     class Menu
     {
+        private string Name { get; set; }
+        private string Leves { get; set; }
+        private string Foetel { get; set; }
+        private string Desszert { get; set; }
+        private string Ar { get; set; }
+        private string Kaloria { get; set; }
+
+        public Menu(string name, string leves, string foetel, string desszert, string ar, string kaloria)
+        {
+            Name = name;
+            Leves = leves;
+            Foetel = foetel;
+            Desszert = desszert;
+            Ar = ar;
+            Kaloria = kaloria;
+        }
+        public string getName()
+        {
+            return Name;
+        }
+        public string getLeves()
+        {
+            return Leves;
+        }
+        public string getFoetel()
+        {
+            return Foetel;
+        }
+        public string getDesszert()
+        {
+            return Desszert;
+        }
+        public string getKaloria()
+        {
+            return Kaloria;
+        }
+        public string getAr()
+        {
+            return Ar;
+        }
+        public override string ToString()
+        {
+            return Name + " menu;" + Leves + ";" + Foetel + ";" + Desszert + ";" + Ar + ";" + Kaloria;
+        }
     }
 }
